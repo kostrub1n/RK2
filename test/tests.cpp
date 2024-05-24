eecddaedd1a5978608b1d5b58c68311ef8f30f6a
kostrub1n <144800787+kostrub1n@users.noreply.github.com>
2024-05-24 10:33:31 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,51 +1,24 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "concrete_component.h"
-#include "decorator.h"
 
+using namespace testing;
+
+// Тест для проверки имени напитка
 TEST(HouseBlendTest, NameTest) {
     HouseBlend houseBlend;
     EXPECT_EQ(houseBlend.Name(), "HouseBlend");
 }
 
+// Тест для проверки стоимости напитка
 TEST(HouseBlendTest, CostTest) {
     HouseBlend houseBlend;
     EXPECT_DOUBLE_EQ(houseBlend.Cost(), 30.0);
 }
 
-TEST(DarkRoastTest, NameTest) {
-    DarkRoast darkRoast;
-    EXPECT_EQ(darkRoast.Name(), "DarkRoast");
-}
-
-TEST(DarkRoastTest, CostTest) {
-    DarkRoast darkRoast;
-    EXPECT_DOUBLE_EQ(darkRoast.Cost(), 28.5);
-}
-
-class MockBeverage : public IBeverage {
-public:
-    MOCK_METHOD0(Name, string());
-    MOCK_METHOD0(Cost, double());
-};
-
-TEST(CondimentDecoratorTest, NameTest) {
-    MockBeverage mockBeverage;
-    CondimentDecorator condimentDecorator(&mockBeverage);
-
-    EXPECT_CALL(mockBeverage, Name())
-        .WillOnce(testing::Return("MockBeverage"));
-
-    EXPECT_EQ(condimentDecorator.Name(), "MockBeverage");
-}
-
-TEST(CondimentDecoratorTest, CostTest) {
-    MockBeverage mockBeverage;
-    CondimentDecorator condimentDecorator(&mockBeverage);
-
-    EXPECT_CALL(mockBeverage, Cost())
-        .WillOnce(testing::Return(10.0));
-
-    EXPECT_DOUBLE_EQ(condimentDecorator.Cost(), 10.0);
-    
+// Тест для проверки, что класс HouseBlend является наследником интерфейса IBeverage
+TEST(HouseBlendTest, InheritanceTest) {
+    HouseBlend houseBlend;
+    IBeverage* beverage = dynamic_cast<IBeverage*>(&houseBlend);
+    EXPECT_TRUE(beverage != nullptr);
 }
